e14d37cc9a61ca50261f3ebdf9dc2abe2f1e31bc
spencer0124 <spencer0124@naver.com>
2024-07-20 23:28:13 +0900

    Initial Commit

---
+++ eyeson/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eyeson
+//
+//  Created by 조승용 on 7/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
