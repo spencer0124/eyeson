9691cb5784793f2342d7135e52075fb7660959d9
spencer0124 <spencer0124@naver.com>
2024-07-22 12:29:24 +0900

    add: core bluetooth

--- eyeson/bluetooth/bluetoothScan.swift
+++ eyeson/bluetooth/bluetoothScan.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct bluetoothScan: View {
+    @StateObject var bleManager = BLEManager()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Text("Bluetooth devices")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            List(bleManager.peripherals) { peripheral in
+                HStack {
+                    Text(peripheral.name)
+                    Spacer()
+                    Text(String(peripheral.rssi))
+                    Button(action: {
+                        bleManager.connect(to: peripheral)
+                    }) {
+                        if bleManager.connectedPeripheralUUID == peripheral.id {
+                            Text("Connected")
+                                .foregroundColor(.green)
+                        } else {
+                            Text("Connect")
+                        }
+                    }
+                }
+            }
+            .frame(height: UIScreen.main.bounds.height / 2)
+            
+            Spacer()
+            
+            Text("STATUS")
+                .font(.headline)
+            
+            if bleManager.isSwitchedOn {
+                Text("Bluetooth is switched on")
+                    .foregroundColor(.green)
+            } else {
+                Text("Bluetooth is NOT switched on")
+                    .foregroundColor(.red)
+            }
+            
+            Spacer()
+            
+            VStack(spacing: 25) {
+                Button(action: {
+                    bleManager.startScanning()
+                }) {
+                    Text("Start Scanning")
+                }.buttonStyle(BorderedProminentButtonStyle())
+                
+                Button(action: {
+                    bleManager.stopScanning()
+                }) {
+                    Text("Stop Scanning")
+                }.buttonStyle(BorderedProminentButtonStyle())
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .onAppear {
+            if bleManager.isSwitchedOn {
+                bleManager.startScanning()
+            }
+        }
     }
 }
 
--- eyeson/bluetooth/peripheral.swift
+++ eyeson/bluetooth/peripheral.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Peripheral: Identifiable {
+    let id: UUID
+    let name: String
+    let rssi: Int
+    // Declare a constant property 'rssi' of type Int, used for the signal strength of the peripheral
+}
