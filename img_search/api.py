f3f1dbcdd7fd9132c6e0032e32001990f5283058
Minji Park <minjipk.cs@gmail.com>
2024-09-03 22:55:49 +0900

    For debug

--- img_search/api.py
+++ img_search/api.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
-from typing import List
 from dotenv import load_dotenv
 from s3_module import list_images_in_s3, download_image_from_s3
 from cnn_module import load_or_create_feature_vectors, preprocess_query
@@ -60,7 +59,7 @@ async def search_image(file: UploadFile = File(...)):
         results = []
         for n, i in enumerate(indices[0]):
             top_n_id = file_list[i]
-            # print('topn id', top_n_id)
+            print('topn id', top_n_id)
             results.append({
                 'rank': n+1,
                 'file': top_n_id,
