d94e10749f5066d2c162e9adf84901aeb8174e5b
spencer0124 <spencer0124@naver.com>
2025-01-14 03:48:36 +0900

    add: webview chat

--- ios-client/Features/Artwork/View/ArtworkView.swift
+++ ios-client/Features/Artwork/View/ArtworkView.swift
@@ -9,6 +9,155 @@ import SwiftUI
 import UIKit
 import SwiftUIImageViewer
 import Kingfisher
+import SafariServices
+import BetterSafariView
+
+
+
+// 작품 메타 정보 표시 뷰 분리
+struct ArtworkMetaInfoView: View {
+    @ObservedObject var viewModel: DescriptionViewModel
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            HStack {
+                Text("\(viewModel.meta["artist"] ?? "작가 정보 없음")")
+                    .font(.headline)
+                    .accessibilityLabel("작가: \(viewModel.meta["artist"] ?? "정보 없음")")
+
+                Text("\(viewModel.meta["emotion"] ?? "감정 정보 없음")")
+                    .font(.subheadline)
+                    .accessibilityLabel("감정정보: \(viewModel.meta["emotion"] ?? "정보 없음")")
+            }
+
+            Text("\(viewModel.meta["style"] ?? "스타일 정보 없음")")
+                .font(.subheadline)
+                .accessibilityLabel(generateAccessibilityText(for: viewModel.meta["style"] ?? "정보 없음"))
+
+            Spacer()
+                .frame(height: 15)
+        }
+    }
+
+    private func generateAccessibilityText(for text: String) -> String {
+        return text.replacingOccurrences(of: " /", with: ",") // '/' 제거
+            .replacingOccurrences(of: "*", with: " 곱하기 ") // '*' 변환
+            .replacingOccurrences(of: "cm", with: "센치미터")
+    }
+}
+
+struct ArtworkImageView: View {
+    @ObservedObject var viewModel: DescriptionViewModel
+    @Binding var isImagePresented: Bool
+    private var closeButton: some View {
+        Button {
+            isImagePresented = false
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        }
+        .buttonStyle(.bordered)
+        .clipShape(Circle())
+        .tint(.purple)
+        .padding()
+    }
+
+    var body: some View {
+        HStack {
+            Spacer()
+            KFImage(URL(string: viewModel.image_url))
+                .placeholder {
+                    ProgressView()
+                        .frame(width: 65, height: 55)
+                }
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(12)
+                .padding()
+                .onTapGesture {
+                    isImagePresented = true
+                }
+                .fullScreenCover(isPresented: $isImagePresented) {
+                    ZStack {
+                        Color.white
+                        ZStack {
+                            SwiftUIImageViewer(imageURLString: viewModel.image_url)
+                                .accessibilityLabel("\(viewModel.meta["title"] ?? " ") 작품 이미지")
+                            VStack {
+                                Spacer()
+                                    .frame(height: 10)
+                                HStack {
+                                    Spacer()
+                                    closeButton
+                                        .accessibilityLabel("닫기 버튼")
+                                        .accessibilityHint("작품 상세정보 화면으로 돌아간다")
+                                    Spacer()
+                                        .frame(width: 10)
+                                }
+                                Spacer()
+                            }
+                        }
+                    }
+                }
+        }
+        .accessibilityLabel("작품 이미지")
+        .accessibilityHint("해설을 원하는 부분을 확대한 뒤, 해설 요청하기 버튼을 클릭하세요")
+    }
+}
+
+// 버튼들을 포함하는 별도의 뷰 정의
+struct ActionButtonsView: View {
+    @Binding var isImagePresented: Bool
+    @Binding var showSafari: Bool
+    @ObservedObject var viewModel: DescriptionViewModel
+
+
+    var body: some View {
+        VStack(spacing: 10) {
+            Button(action: {
+                isImagePresented = true
+            }) {
+                Text("작품 확대하여 해설 요청하기")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .background(Color(hex: "404293"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 20)
+            .accessibilityLabel("작품 확대하여 해설보기")
+            .accessibilityHint("그림을 누르면 확대/축소나 자세한 설명 듣기가 가능합니다")
+            .accessibilityAddTraits(.isButton)
+
+            Button(action: {
+                showSafari = true
+            }) {
+                Text("작품 감상평 공유하기")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .background(Color(hex: "404293"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 20)
+            .accessibilityLabel("작품 감상평 공유하기") // 레이블 수정
+            .accessibilityHint("작품 감상평을 공유하기 위해 웹페이지를 엽니다.") // 힌트 수정
+            .accessibilityAddTraits(.isButton)
+            .fullScreenCover(isPresented: $showSafari) {
+                if let artworkId = viewModel.meta["title"],
+                   let url = URL(string: "http://43.201.93.53:8000/?museum=1&artworkid=\(artworkId)") {
+                    SafariView(
+                        url: url,
+                        configuration: SafariView.Configuration(
+                            entersReaderIfAvailable: false,
+                            barCollapsingEnabled: true
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
 
 struct ArtworkView: View {
     var eng_id: String
@@ -18,6 +167,9 @@ struct ArtworkView: View {
     @State private var isImagePresented = false
     @State private var isBottomSheetPresented = false
     @State private var capturedImage: UIImage? = nil
+    
+    // 웹뷰 관련
+    @State var showSafari = false
    
 
     var body: some View {
@@ -31,158 +183,22 @@ struct ArtworkView: View {
                     .foregroundColor(.red)
             } else {
                 
-                VStack (alignment: .leading, spacing: 5) {
-                    HStack {
-                        
-                        Text("\(viewModel.meta["artist"] ?? "작가 정보 없음")")
-                            .font(.headline)
-                            .accessibilityLabel("작가: \(viewModel.meta["artist"] ?? "정보 없음")")
-                        
-                        Text("\(viewModel.meta["emotion"] ?? "감정 정보 없음")")
-                            .font(.subheadline)
-                            .accessibilityLabel("감정정보: \(viewModel.meta["emotion"] ?? "정보 없음")")
-                    }
-                    
-                    Text("\(viewModel.meta["style"] ?? "스타일 정보 없음")")
-                        .font(.subheadline)
-                        .accessibilityLabel(generateAccessibilityText(for: viewModel.meta["style"] ?? "정보 없음"))
-                    
-                    Spacer()
-                        .frame(height: 15)
-                }
+                ArtworkMetaInfoView(viewModel: viewModel)
                 
                 
                 VStack (alignment: .leading, spacing: 20) {
                     
                     Divider()
                 
-                    HStack {
-                        Spacer()
-                        KFImage(URL(string: viewModel.image_url))
-                                
-                                .placeholder {
-                                ProgressView()
-                                    .frame(width: 65, height: 55)
-                                }
-                                .resizable()
-                                .scaledToFit()
-                                .cornerRadius(12)
-                                .padding()
-                                .onTapGesture {
-                                            isImagePresented = true
-                                        }
-                                
-                                .fullScreenCover(isPresented: $isImagePresented) {
-                                    ZStack {
-                                            Color.white
-                                        
-                                        ZStack {
-                                            SwiftUIImageViewer(imageURLString: viewModel.image_url)
-                                                .accessibilityLabel("\(viewModel.meta["title"] ?? " ") 작품 이미지")
-                                            
-                                            VStack {
-                                                Spacer()
-                                                    .frame(height: 10)
-                                                HStack {
-                                                    Spacer()
-                                                    closeButton
-                                                        .accessibilityLabel("닫기 버튼")
-                                                        .accessibilityHint("작품 상세정보 화면으로 돌아간다")
-                                                    Spacer()
-                                                        .frame(width: 10)
-                                                }
-                                                Spacer()
-                                            }
-                                            
-                                        }
-                                        
-                                        
-                                                
-                                                
-                                            VStack {
-                                                Spacer()
-                                                Button(action: {
-                                                    withAnimation {
-                                                        captureScreenshotAndRequestDescription()
-                                                        
-                                                            isBottomSheetPresented = true
-                                                    }
-                                                }, label: {
-                                                    Text("해설 요청하기")
-//                                                        .font(.system(size: 15))
-                                                        .foregroundColor(.white)
-                                                        .padding(.horizontal, 30)
-                                                        .padding(.vertical, 20)
-                                                        .background(
-                                                                            RoundedRectangle(cornerRadius: 10) // 배경 모양
-                                                                                .stroke(
-                                                                                    AngularGradient( // 각진 그라데이션
-                                                                                        gradient: Gradient(colors: [.red, .yellow]), // 색상
-                                                                                        center: .center, // 중심
-                                                                                        startAngle: .degrees(0), // 시작 각도
-                                                                                        endAngle: .degrees(360) // 끝 각도
-                                                                                    ),
-                                                                                    lineWidth: 10 // 테두리 두께
-                                                                                )
-                                                                                .background(Color(hex: "404293")) // 내부 배경색
-                                                                        )
-                                                        .cornerRadius(10)
-                                                })
-                                                .accessibilityHidden(isBottomSheetPresented)
-                                                .accessibilityLabel("해설 요청") // 버튼 역할 설명
-                                                .accessibilityHint("현재 화면에서 확대한 부분에 대해 자세하게 설명해줍니다") // 동작에 대한 힌트 추가
-                                                .accessibilityAddTraits(.isButton) // 접근성 요소에 버튼 특성 추가
-                                                .contentShape(Rectangle()) // 더 큰 터치 영역 제공
-                                                
-                                                Spacer()
-                                                    .frame(height: 100)
-                                            }
-                                        }
-                                        .overlay(
-                                            VStack {
-                                                Spacer()
-                                                if isBottomSheetPresented {
-                                                    bottomSheet
-                                                        .transition(.move(edge: .bottom))
-                                                        .animation(.easeInOut)
-                                                }
-                                                
-                                            }
-                                        )
-                                    
-                                    // ✅ alert modifier 추가
-//                                           .alert(isPresented: $viewModel.isDescriptionLoaded) {
-////                                               $viewModel.isDescriptionLoaded = false
-//                                               if let error = viewModel.descriptionRequestError {
-//                                                   return Alert(title: Text("오류"), message: Text(error), dismissButton: .default(Text("확인")))
-//                                               } else {
-//                                                   return Alert(title: Text("해설 완료"), message: Text("해설 생성이 완료되었습니다."), dismissButton: .default(Text("확인")))
-//                                               }
-//
-//                                           }
-                                    }
-                                
-                    }
-                    .accessibilityLabel("작품 이미지")
-                    .accessibilityHint("해설을 원하는 부분을 확대한 뒤, 해설 요청하기 버튼을 클릭하세요")
+                    ArtworkImageView(viewModel: viewModel, isImagePresented: $isImagePresented)
                     
-             
-                    Button(action: {
-                        isImagePresented = true
-//                        print("clicked")
-                    }) {
-                        Text("작품 확대하여 해설 요청하기")
-//                            .font(.system(size: 15))
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 10)
-                            .background(Color(hex: "404293"))
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    .padding(.horizontal, 20)
-                    .accessibilityLabel("작품 확대하여 해설보기") // 버튼 역할 설명
-                    .accessibilityHint("그림을 누르면 확대/축소나 자세한 설명 듣기가 가능합니다") // 동작에 대한 힌트 추가
-                    .accessibilityAddTraits(.isButton) // 접근성 요소에 버튼 특성 추가
+                    ActionButtonsView(
+                                isImagePresented: $isImagePresented,
+                                showSafari: $showSafari,
+                                viewModel: viewModel
+                                        )
+                    
+                    
                     
                     
                     
@@ -218,6 +234,7 @@ struct ArtworkView: View {
             viewModel.fetchDescription(for: eng_id)
         }
     }
+    
     
     
     private var closeButton: some View {
