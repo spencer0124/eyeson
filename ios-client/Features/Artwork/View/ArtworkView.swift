b81889b4b50356f48c388e6652e6a1d2fc6c78b1
kimcorn02 <mamapapai@g.skku.edu>
2025-01-20 11:53:59 +0900

    Edit: artwork id from app

--- ios-client/Features/Artwork/View/ArtworkView.swift
+++ ios-client/Features/Artwork/View/ArtworkView.swift
@@ -283,17 +283,28 @@ struct ActionButtonsView: View {
             .accessibilityHint("작품 감상평을 공유하기 위해 웹페이지를 엽니다.") // 힌트 수정
             .accessibilityAddTraits(.isButton)
             .fullScreenCover(isPresented: $showSafari) {
-                if let artworkId = viewModel.meta["title"],
-                   let url = URL(string: "http://43.201.93.53:8000/?museum=\(paramUniqueId)&artworkid=\(artworkId)") {
-//                    print("webviewurl: \(url)")
-                    SafariView(
-                        url: url,
-                        configuration: SafariView.Configuration(
-                            entersReaderIfAvailable: false,
-                            barCollapsingEnabled: true
+//                 if let artworkId = viewModel.meta["title"],
+//                    let url = URL(string: "http://43.201.93.53:8000/?museum=\(paramUniqueId)&artworkid=\(artworkId)") {
+// //                    print("webviewurl: \(url)")
+//                     SafariView(
+//                         url: url,
+//                         configuration: SafariView.Configuration(
+//                             entersReaderIfAvailable: false,
+//                             barCollapsingEnabled: true
+//                         )
+//                     )
+//                 }
+                    if let artworkId = viewModel.eng_id,
+                        let url = URL(string: "http://43.201.93.53:8000/?museum=\(paramUniqueId)&artworkid=\(artworkId)") {
+    //                    print("webviewurl: \(url)")
+                        SafariView(
+                            url: url,
+                            configuration: SafariView.Configuration(
+                                entersReaderIfAvailable: false,
+                                barCollapsingEnabled: true
+                            )
                         )
-                    )
-                }
+                    }
             }
             
 //            .fullScreenCover(isPresented: $showSafari) {
