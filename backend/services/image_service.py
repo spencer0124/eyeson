0ab9371c8260d8caf98a8a00b27656563f7fd755
kimcorn02 <mamapapai@g.skku.edu>
2025-01-22 20:19:11 +0900

    Add: title to engid

--- backend/services/image_service.py
+++ backend/services/image_service.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from PIL import Image
+import json
 
 def dtype_is(img_path: str) -> str:
     if img_path[-3:].lower() == 'jpg':
@@ -13,3 +14,16 @@ def image_to_bytes(image: Image) -> bytes:
     byte_io = BytesIO()
     image.save(byte_io, format=image.format)
     return byte_io.getvalue()
+
+def load_data(path):
+    with open(path, "r") as f:
+        return json.load(f)
+
+def load_id_from_title(title: str):
+    data_path = './data/data.json'
+    json_data = load_data(data_path)
+
+    for item in json_data.get("meta", []):  # 'meta' 리스트 탐색
+        if item.get("title") == title:
+            return json_data.get("eng_id")  # 해당 제목의 eng_id 반환
+    raise ValueError(f"Title '{title}' not found in JSON data.")
